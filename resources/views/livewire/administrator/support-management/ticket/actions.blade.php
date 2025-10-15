cbc98cee83be1bc85309eceb13d616286ced893c
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-10-15 17:40:56 +0330

    "Replace modal-based ticket view with direct link to ticket details page"

--- resources/views/livewire/administrator/support-management/ticket/actions.blade.php
+++ resources/views/livewire/administrator/support-management/ticket/actions.blade.php
@@ -1,10 +1,10 @@
 <div class="flex gap-2">
     <!-- Edit -->
-    <button type="button"
+    <a href="{{ route('administrator.support-management.ticket.view', ['ticketId' => $ticket->id]) }}"
             class="px-3 py-2 text-xs font-medium text-center inline-flex items-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            x-modal:open.preload="{ component: 'platform.administrator.support-management.ticket.view', props: { ticketId: {{ $ticket->id }} } }">
+>
         {{ __('platform::common.view') }}
-    </button>
+    </a>
 
 
     <!-- Delete -->
