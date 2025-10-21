03f607d87d375a5b97c6417ec9e38323a570f1d3
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-10-21 13:03:43 +0330

    Add Activity LogManagement detail view and corresponding Livewire component

---
+++ lang/fa/permissions.php
@@ -0,0 +1,69 @@
+<?php
+return [
+    'user' => [
+        'user_access' => 'User Access',
+        'user_dashboard' => 'User Dashboard',
+        'user_settings' => 'User Settings',
+        'user_support' => 'User Support',
+        'user_support_ticket_index' => 'User Support Ticket Index',
+        'user_support_ticket_create' => 'User Support Ticket Create',
+    ],
+    'administrator' => [
+        'administrator_access' => 'Administrator Access',
+
+        'administrator_dashboard_index' => 'Administrator Dashboard Index',
+        'administrator_user_management' => 'Administrator User Management',
+
+        'administrator_user_index' => 'Administrator User Index',
+        'administrator_user_create' => 'Administrator User Create',
+        'administrator_user_edit' => 'Administrator User Edit',
+        'administrator_user_delete' => 'Administrator User Delete',
+        'administrator_user_roles' => 'Administrator User Roles',
+        'administrator_user_permissions' => 'Administrator User Permissions',
+
+        'administrator_user_role_index' => 'Administrator User Role Index',
+        'administrator_user_role_create' => 'Administrator User Role Create',
+        'administrator_user_role_edit' => 'Administrator User Role Edit',
+        'administrator_user_role_delete' => 'Administrator User Role Delete',
+        'administrator_user_role_users' => 'Administrator Role Users',
+        'administrator_user_role_permissions' => 'Administrator Role Permissions',
+
+        'administrator_user_permission_index' => 'Administrator User Permission Index',
+        'administrator_user_permission_create' => 'Administrator User Permission Create',
+        'administrator_user_permission_edit' => 'Administrator User Permission Edit',
+        'administrator_user_permission_delete' => 'Administrator User Permission Delete',
+
+        'administrator_content_management' => 'Administrator Content Management',
+
+        'administrator_content_category_index' => 'Administrator Content Management Category Index',
+        'administrator_content_category_create' => 'Administrator Content Management Category Create',
+        'administrator_content_category_edit' => 'Administrator Content Management Category Edit',
+        'administrator_content_category_delete' => 'Administrator Content Management Category Delete',
+
+        'administrator_content_faq_index' => 'Administrator Content Management FAQ Index',
+        'administrator_content_faq_create' => 'Administrator Content Management FAQ Create',
+        'administrator_content_faq_edit' => 'Administrator Content Management FAQ Edit',
+        'administrator_content_faq_delete' => 'Administrator Content Management FAQ Delete',
+
+        'administrator_content_article_index' => 'Administrator Content Management Article Index',
+        'administrator_content_article_create' => 'Administrator Content Management Article Create',
+        'administrator_content_article_edit' => 'Administrator Content Management Article Edit',
+        'administrator_content_article_delete' => 'Administrator Content Management Article Delete',
+
+        'administrator_support_management' => 'Administrator Support Management',
+        'administrator_support_ticket_index' => 'Administrator Support Ticket Index',
+        'administrator_support_ticket_view' => 'Administrator Support Ticket View',
+        'administrator_support_ticket_replay' => 'Administrator Support Ticket Replay',
+
+
+        'administrator_setting_management' => 'Administrator Setting Management',
+
+        'administrator_setting_category_index' => 'Administrator Setting Management Category Index',
+        'administrator_setting_category_create' => 'Administrator Setting Management Category Create',
+        'administrator_setting_category_edit' => 'Administrator Setting Management Category Edit',
+        'administrator_setting_category_delete' => 'Administrator Setting Management Category Delete',
+
+        'administrator_setting_option_index' => 'Administrator Setting Management Option Index',
+        'administrator_setting_function_index' => 'Administrator Setting Management Function Index',
+    ],
+];
